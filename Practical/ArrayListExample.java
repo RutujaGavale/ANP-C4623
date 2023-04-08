7bae8f67f5e7dfbb7568ef6369cd5210c6b70cc3
RutujaGavale <126570159+RutujaGavale@users.noreply.github.com>
2023-04-08 16:52:33 +0530

    Add files via upload

---
+++ Practical/ArrayListExample.java
@@ -0,0 +1,21 @@
+package collectionframework;
+
+import java.util.*;
+
+public class ArrayListExample 
+{
+
+	public static void main(String[] args)
+	{
+		ArrayList<String>arraylist=new ArrayList<String>();
+		//adding object in arraylist
+		arraylist.add("mango");
+		arraylist.add("banana");
+		arraylist.add("mango");
+		arraylist.add("orange");
+		arraylist.add("apple");
+		
+		
+	}
+
+}
---
+++ Practical/LinkedListEx.java
@@ -0,0 +1,26 @@
+package collectionframework;
+
+//package for collection framework in java
+import java.util.*;
+
+public class LinkedListEx 
+{
+	//main method
+	public static void main(String[] args)
+	{
+		LinkedList<Integer>list=new LinkedList<Integer>();
+		//adding object in linked list
+		
+		list.add(45);
+		list.add(76);
+		list.add(56);
+		list.add(89);
+		list.add(45);
+		//printing elements of linked list
+		System.out.println("elements:"+ list);
+		
+			
+
+
+	}
+}
